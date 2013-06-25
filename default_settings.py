830c2428a704a41efcd77712f9f02e5c2500ff9e
James Rutherford <james.rutherford@maplecroft.com>
2013-06-25 16:47:56 +0100

    fixed default settings

--- default_settings.py
+++ default_settings.py
@@ -1,5 +1,5 @@
 batik_path = 'org.apache.batik.apps.rasterizer.Main'
 java_cmd_format = (
-        "java {batik_path} {type_option} -d {out_filename} {width_option}"
+        "java {batik_path} {type_option} -d {out_filename} {width_option} "
         "-bg {background} -dpi {dpi} {temp_svg_name}"
 )
